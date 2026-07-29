653580c4eb2cb08db70b10c1297fdb786d8faf87
tayyabmemon2808 <tayyabmemon2808gmail.com>
2026-07-29 14:48:59 +0500

    booking-request-model

---
+++ backend/models/BookingRequest.js
@@ -0,0 +1,68 @@
+const mongooose = require("mongoose");
+
+const bookingRequestSchema = new mongooose.Schema({
+ name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  university: {
+    type: String,
+    required: true
+  },
+  hostel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hostel',
+    required: true
+  },
+  roomType: {
+    type: String,
+    enum: ['single', 'shared'],
+    required: true
+  },
+  checkInDate: {
+    type: Date,
+    required: true
+  },
+  checkOutDate: {
+    type: Date,
+    required: true
+  },
+  calculatedPrice: {
+    type: Number,
+    required: true
+  },
+  paymentMethod: {
+    type: String,
+    enum: ['jazzcash', 'easypaisa', 'bank_transfer'],
+    required: true
+  },
+  paymentReference: {
+    type: String,
+    required: true
+  },
+  referenceId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: 'pending'
+  }
+},{
+    timestamps: true
+});
+module.exports = mongoose.model("BookingRequest" , bookingRequestSchema)
